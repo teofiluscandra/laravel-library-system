f995cc3a3f62b3bf9ba6b9e2d9cb9b4121e0a0e5
teofiluscandra <teofiluschandra@gmail.com>
2017-07-10 14:02:47 +0800

    Finishing

--- database/seeds/UsersSeeder.php
+++ database/seeds/UsersSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Role;
 use App\User;
+use App\Staff;
+use App\Member;
 
 class UsersSeeder extends Seeder
 {
@@ -33,7 +35,7 @@ class UsersSeeder extends Seeder
 
         // Membuat sample admin
         $admin = new User();
-        $admin->name = 'Admin Dinas Kearsipan dan Perpustakaan Provinsi Bali';
+        $admin->name = 'Kepala Perpustakaan';
         $admin->email = 'admin@gmail.com';
         $admin->password = bcrypt('rahasia');
         $admin->role = 'admin';
@@ -51,6 +53,16 @@ class UsersSeeder extends Seeder
         $member->save();
         $member->attachRole($memberRole);
 
+        $member_data = new Member();
+        $member_data->user_id = $member->id;
+        $member_data->kode_member = "KD0001";
+        $member_data->tempat_lahir = "Denpasar";
+        $member_data->tanggal_lahir = "1994-01-01";
+        $member_data->no_identitas = "123-123-123";
+        $member_data->alamat = "Sesetan";
+        $member_data->jenis_kelamin = "Laki-laki";
+        $member_data->save();
+
         // Membuat sample member
         $staff = new User();
         $staff->name = "Sample Staff";
@@ -60,5 +72,13 @@ class UsersSeeder extends Seeder
         $staff->is_verified = 1;
         $staff->save();
         $staff->attachRole($staffRole);
+
+        $staff_data = new Staff();
+        $staff_data->user_id = $staff->id;
+        $staff_data->nip = "123-123";
+        $staff_data->telp = "087858687999";
+        $staff_data->alamat = "Sesetan";
+        $staff_data->jenis_kelamin = "Laki-laki";
+        $staff_data->save();
     }
 }
